2acca459b347b06f7a9a6319bdec9e55bcc82296
lab <navot@volcani.agri.gov.il>
2021-10-05 10:46:44 +0300

    Tau2Grabber.py added lens_number to params setter

--- devices/Camera/Tau/Tau2Grabber.py
+++ devices/Camera/Tau/Tau2Grabber.py
@@ -93,6 +93,7 @@ class Tau2Grabber(Tau):
         self.brightness_bias = params.get('brightness_bias', 0)
         self.cmos_depth = params.get('cmos_depth', 0)  # 14bit pre AGC
         self.fps = params.get('fps', ptc.FPS_CODE_DICT[60])  # 60Hz NTSC
+        self.lens_number = params.get('lens_number', 1)
         ####### self.correction_mask = params.get('corr_mask', 0)  # Always OFF!!!
 
     def _th_reader_func(self) -> None:
