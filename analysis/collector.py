e73e420f2b0f21018540e0be24af843d21364abd
navotLab <asfir29@gmail.com>
2021-10-05 11:56:35 +0300

    collector.py added prints for termination.

--- analysis/collector.py
+++ analysis/collector.py
@@ -137,10 +137,17 @@ if __name__ == "__main__":
             idx += 1
     try:
         camera.terminate()
+        print('Camera terminated.')
     except (ValueError, TypeError, AttributeError, RuntimeError, NameError, KeyError, AssertionError):
         pass
     try:
         blackbody.__del__()
+        print('BlackBody terminated.')
+    except (ValueError, TypeError, AttributeError, RuntimeError, NameError, KeyError, AssertionError):
+        pass
+    try:
+        del filterwheel
+        print('Filterwheel terminated.')
     except (ValueError, TypeError, AttributeError, RuntimeError, NameError, KeyError, AssertionError):
         pass
     [p.join() for p in list_threads]
