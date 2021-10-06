793b261bd1f13d1af5728b15c4a66105d5ff1f3e
lab <navot@volcani.agri.gov.il>
2021-10-06 08:20:40 +0300

    collector.py tqdm without tabs

--- analysis/collector.py
+++ analysis/collector.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
             sleep(1)  # clears the buffer after the FFC
             with tqdm(total=n_images) as progressbar:
                 progressbar.set_description_str(f'Filter {filter_name}nm')
-                progressbar.set_postfix_str(f'\tBlackBody {t_bb}C\t\tMeasurements: {idx}|{length_total}')
+                progressbar.set_postfix_str(f'BlackBody {t_bb}C, Measurements: {idx}|{length_total}')
                 while len(dict_images[t_bb][filter_name]) != n_images:
                     dict_images[t_bb][filter_name].append(camera.image)
                     dict_fpa.setdefault(t_bb, {}).setdefault(filter_name, []).append(camera.fpa)
